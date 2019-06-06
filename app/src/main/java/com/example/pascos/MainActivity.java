dd04fd758ded2dd53475110f3cb1a78cbc400e3e
mews2 <mews2000@gmail.com>
2019-06-06 12:41:18 +0900

    comcom

--- app/src/main/java/com/example/pascos/MainActivity.java
+++ app/src/main/java/com/example/pascos/MainActivity.java
@@ -1,67 +1,99 @@
 package com.example.pascos;
 
 import android.bluetooth.BluetoothAdapter;
+
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.app.Activity;
 
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
 
 
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 import app.akexorcist.bluetotohspp.library.BluetoothState;
 import app.akexorcist.bluetotohspp.library.DeviceList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int MILLISINFUTURE = 10010;
     private static final int COUNT_DOWN_INTERVAL = 10;
-
+    private static final int LAYOUT = R.layout.activity_main;
     private int count = 7000;
-    private TextView countTxt ;
+    private TextView countTxt;
     private TextView amm;
     private CountDownTimer countDownTimer;
     private BluetoothSPP bt;
 
+    private Button personal;
+    private String requestName;
+    private Button amount;
+
+
+    private TextView thermotext;
+    private String nameEt;
+    private int amo;
+    private TextView thermot;
+    private TextView humit;
+    private TextView dirtt;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
-        countTxt = (TextView)findViewById(R.id.count_txt);
-        amm = (TextView) findViewById(R.id.amm);
-        Intent intent = getIntent();
+        personal = (Button) findViewById(R.id.personal);
+        countTxt = (TextView) findViewById(R.id.count_txt);
+
+        thermot = (TextView) findViewById(R.id.thermot);
+        humit = (TextView) findViewById(R.id.humit);
+        dirtt = (TextView) findViewById(R.id.dirtt);
+        amount = (Button) findViewById(R.id.amount);
+
         countDownTimer();
 
-        String dd = intent.getStringExtra("hei".toString());
-        amm.setText(dd + "ml");
 
 
 
-
-        ImageButton bluetooth = (ImageButton) findViewById(R.id.btnConnect);
-        Button personal = (Button) findViewById(R.id.personal);
         Button kill = (Button) findViewById(R.id.kill);
 
-        personal.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,wheight.class);
-                startActivity(intent);
-            }
-        });
+
+
+        personal.setOnClickListener(this);
+        amount.setOnClickListener(this);
+        thermotext = (TextView) findViewById(R.id.thermotext);
+
+        thermot.setText("온도 : "+"℃");
+        humit.setText("습도 : "+"%");
+        dirtt.setText("미세먼지 : "+"㎍/㎥");
+
+
+
+
+
+
+
+
+
+
 
         bt = new BluetoothSPP(this); //Initializing
 
@@ -74,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
         bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
             public void onDataReceived(byte[] data, String message) {
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -96,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ImageButton btnConnect = findViewById(R.id.btnConnect); //연결시도
+        ImageView btnConnect = findViewById(R.id.btnb); //연결시도
         btnConnect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
@@ -112,6 +144,52 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.personal:
+
+                CustomDialog dialog = new CustomDialog(this, requestName);
+                dialog.setDialogListener(new MyDialogListener() {
+                    @Override
+                    public void onPositiveClicked(String email, String name) {
+
+                            setResult(email, name);
+
+                    }
+
+                    @Override
+                    public void onNegativeClicked() {
+                        Log.d("MyDialogListener", "onNegativeClicked");
+
+                    }
+                });
+                dialog.show();
+                break;
+            case R.id.amount:
+                requestName = Integer.toString(amo);
+                CustomDialogDrink dialogDrink = new CustomDialogDrink(this, requestName);
+                dialogDrink.setPopup(new Mypopup() {
+                    @Override
+                    public void confiremd() {
+                        Log.d("Mypopup", "confirmed");
+                    }
+                });
+
+
+
+                dialogDrink.show();
+                break;
+        }
+    }
+
+
+
+    private void setResult(String email,String name) {
+        amo = (Integer.parseInt(email)+Integer.parseInt(name))*10 ;
 
     }
 
@@ -180,26 +258,26 @@ public class MainActivity extends AppCompatActivity {
         Button kill = findViewById(R.id.kill); //데이터 전송
         final Handler handler = new Handler();
         kill.setOnClickListener(new View.OnClickListener() {
-                                       public void onClick(View v) {
-                                           if(count == 7000) {
-                                               countDownTimer.start();
+            public void onClick(View v) {
+                if (count == 7000) {
+                    countDownTimer.start();
 
-                                               ProgressBar timer = (ProgressBar) findViewById(R.id.timer);
-                                               ProgressBar gage = (ProgressBar) findViewById(R.id.gage);
-                                               gage.setProgress(0);
-                                               timer.setProgress(count);
-                                               bt.send("1", true);
-                                               handler.postDelayed(new Runnable() {
-                                                   @Override
-                                                   public void run() {
-                                                       bt.send("0", true);
-                                                   }
-                                               }, 10000);
-                                           }
+                    ProgressBar timer = (ProgressBar) findViewById(R.id.timer);
+                    ProgressBar gage = (ProgressBar) findViewById(R.id.gage);
+                    gage.setProgress(0);
+                    timer.setProgress(count);
+                    bt.send("1", true);
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            bt.send("0", true);
+                        }
+                    }, 10000);
+                }
 
-                                       }
-                                   }
-        );
+            }
+
+        });
 
     }
 
